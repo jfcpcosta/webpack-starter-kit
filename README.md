5a5c313fae87de28695d9dcab3378ecab79bc39a
Francisco Costa <jfcpcosta@gmail.com>
2020-04-10 01:02:21 +0100

    Update dependencies. Change default layout and styles. Add primitive instructions on README file.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Webpack Starter Kit
+
+## Getting start
+Start project using command:
+
+    npm run start:dev 
+    yarn start:dev
+
+## Build
+Build project using command:
+
+    npm run build
+    yarn build
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './styles/main.scss';
+import { greeting } from './scripts/greeting'
 
-import {
-    greeting
-} from './scripts/greeting'
-
-console.log(greeting('John Doe'));
+document.querySelector('code#message').textContent = greeting('John Doe');
