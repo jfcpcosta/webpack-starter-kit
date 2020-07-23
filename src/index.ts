a3f5c2b7fe88f5d2c2dd15d53874cb6196a27226
Francisco Costa <jfcpcosta@gmail.com>
2020-07-23 12:03:20 +0100

    add simple frontend router

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import './styles/main.scss';
 import { greeting } from './greeting'
+import Router, { RouterMode } from './router';
 
-document.querySelector('code#message').textContent = greeting('John Doe');
+const router = new Router({
+    mode: RouterMode.HASH,
+    root: '/'
+});
+
+router
+  .add(/about/, () => {
+    document.querySelector('code#message').textContent = 'welcome in about page';
+  })
+  .add(/products\/(.*)\/specification\/(.*)/, (id, specification) => {
+    document.querySelector('code#message').textContent = `products: ${id} specification: ${specification}`;
+  })
+  .add('', () => {
+    document.querySelector('code#message').textContent = greeting('John Doe');
+  });
+
+document.querySelectorAll('a').forEach(link => {
+    router.navigate(link.href);
+    return false;
+});
